dd62027ad8618254b8248ab0dc3e9a0716507858
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-13 17:56:43 +0900

    コントローラとルートを整理

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,31 @@
 class TweetsController < ApplicationController
+
+  def index
+
+  end
+
+  def create 
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destory
+
+  end
+
+  def confirm
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :tweets
+
+  resources :tweets do
+    collection do
+      post :confirm
+    end
+  end
 end
